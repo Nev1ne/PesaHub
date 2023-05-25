abb59d2ed8b9dc733f9c1c473c26b54a8dea06d5
Nevine Achola <nevine.achola@student.moringaschool.com>
2023-05-25 20:30:41 +0300

    final js commit

--- assets/js/init-alpine.js
+++ assets/js/init-alpine.js
@@ -1,6 +1,6 @@
 function data() {
     function getThemeFromLocalStorage() {
-      // if user already changed the theme, use it
+      // if user has already changed the theme, maintain it
       if (window.localStorage.getItem('dark')) {
         return JSON.parse(window.localStorage.getItem('dark'))
       }
@@ -29,3 +29,34 @@ function data() {
       closeSideMenu() {
         this.isSideMenuOpen = false
       },
+      isNotificationsMenuOpen: false,
+      toggleNotificationsMenu() {
+        this.isNotificationsMenuOpen = !this.isNotificationsMenuOpen
+      },
+      closeNotificationsMenu() {
+        this.isNotificationsMenuOpen = false
+      },
+      isProfileMenuOpen: false,
+      toggleProfileMenu() {
+        this.isProfileMenuOpen = !this.isProfileMenuOpen
+      },
+      closeProfileMenu() {
+        this.isProfileMenuOpen = false
+      },
+      isPagesMenuOpen: false,
+      togglePagesMenu() {
+        this.isPagesMenuOpen = !this.isPagesMenuOpen
+      },
+      // Modal
+      isModalOpen: false,
+      trapCleanup: null,
+      openModal() {
+        this.isModalOpen = true
+        this.trapCleanup = focusTrap(document.querySelector('#modal'))
+      },
+      closeModal() {
+        this.isModalOpen = false
+        this.trapCleanup()
+      },
+    }
+  }
