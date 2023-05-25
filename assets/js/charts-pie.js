e01a7595ee526d2a0aa55b3fdd08b44b56f322b4
Nevine Achola <nevine.achola@student.moringaschool.com>
2023-05-25 20:25:53 +0300

    updated js

--- assets/js/charts-pie.js
+++ assets/js/charts-pie.js
@@ -27,7 +27,7 @@ const pieConfig = {
     },
   }
   
-  // change this to the id of your chart element in HMTL
+  
   const pieCtx = document.getElementById('pie')
   window.myPie = new Chart(pieCtx, pieConfig)
   
